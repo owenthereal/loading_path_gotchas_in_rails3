1d53fb3000a632f3105eccea7d95d5d48a58da44
Jingwen Owen Ou <jingweno@gmail.com>
2011-01-20 11:33:11 -0800

    init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoloadInRails3::Application.config.secret_token = '408b4792a7e8e7add7196a22a15084688672fa8d889f33f5faae46d20f64b314252af19ad82afe04327bdf0694f109517b3fb669cfa79f62018feec42fdf5735'
